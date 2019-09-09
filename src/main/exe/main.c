6cb49f09f0240275e490915ad43331539406bcb3
Michael Brockus <squidfarts@icloud.com>
2019-09-08 20:44:18 -0700

    First commit.

---
+++ src/main/exe/main.c
@@ -0,0 +1,26 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.c                                                                    *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: Apache 2.0 :http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                                                 *
+ ***********************************************************************************/
+#include "project.h"
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, Meson' is displayed to the standard console.
+ *
+ */
+int main(void)
+{
+    puts("Hello, Meson.");
+    
+    return EXIT_SUCCESS;
+}// end of function main
---
+++ src/main/exe/src/project.c
@@ -0,0 +1,19 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: project.c                                                                 *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: Apache 2.0 :http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                                                 *
+ ***********************************************************************************/
+#include "project.h"
+
+const char * itWorks(void)
+{
+    return "It works";
+}// end of functions itWorks
